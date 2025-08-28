51dade99ece4eb6d5eaec83509b6aa4ad447c9bb
Claes Östlund <claesostlund@WF-MBP24.local>
2025-08-28 14:06:03 +0200

    1.0.0

--- PluginFormatDouble.php
+++ PluginFormatDouble.php
@@ -1,6 +1,9 @@
 <?php
 class PluginFormatDouble{
   public function format($d, $decimals = 1){
+    if(!is_numeric($d)){
+      return $d;
+    }
     return sprintf('%0.'.$decimals.'f', $d); // 520 -> 520.00
   }
 }
